1ad8d3cec1a442e53df39e1fc3b4f3292f43d4ef
regan hayes <hayesregan@yahoo.com>
2022-02-02 14:05:57 -0500

    Added modal functionality

--- src/Recipes.js
+++ src/Recipes.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import RenderModalButton from './RenderModalButton';
 import RecipeModal from './RecipeModal';
+import Container from 'react-bootstrap/Container';
 
 
 // let SERVER = process.env.REACT_APP_SERVER;
@@ -14,7 +15,8 @@ class Recipes extends React.Component {
     super(props);
     this.state = {
       recipes: [],
-      show: false
+      show: false,
+      saved: false
     };
   }
   getRecipes = async (url) => {
@@ -22,8 +24,31 @@ class Recipes extends React.Component {
     this.setState({
       recipes: recipeResults.data
     });
-    console.log(this.state.recipes);
   };
+
+  postRecipe = async (savedRecipe) => {
+    try {
+      let url = `http://localhost:3001/recipe`;
+      await axios.post(url, savedRecipe);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  getInstructions = async (id) => {
+    let url = `http://localhost:3001/analyzedInstructions?recipeid=${id}`
+    let instructionsResults = await axios.get(url);
+    this.setState ({
+      instructions: instructionsResults.data
+    });
+    console.log(this.state.instructions);
+  };
+
+  handleGetInstructions = (recipeObj) => {
+    let id = recipeObj.apiId; 
+    console.log(id);
+    this.getInstructions(id);
+  }
 
 
   handleIngredientSubmit = async (e) => {
@@ -33,8 +58,17 @@ class Recipes extends React.Component {
     this.getRecipes(url);
   };
 
+  handlePost = (recipeObj) => {
+    let savedRecipe = recipeObj;
+    // savedRecipe.email = 
+    this.postRecipe(savedRecipe);
+    this.setState({
+      saved: true
+    })
+  }
+
+
   handleShowModal = (recipe) => {
-    console.log('modal activated')
     this.setState({
       show: true,
       currentRecipe: recipe
@@ -49,6 +83,8 @@ class Recipes extends React.Component {
   };
 
   render() {
+
+
     return (
       <>
         <Form onSubmit={this.handleIngredientSubmit}>
@@ -63,26 +99,31 @@ class Recipes extends React.Component {
             Submit
           </Button>
         </Form>
-        {this.state.show && 
-        <RecipeModal 
-          handleCloseModal={this.handleCloseModal}
-          handleShowModal={this.handleShowModal}
-          show={this.state.show}
-          recipeImg={this.state.currentRecipe.image}
-          title={this.state.currentRecipe.title}
-          missedIngredients={this.state.currentRecipe.missedIngredients}
-          recipeObj={this.state.currentRecipe}
-        />}
+        {this.state.show &&
+          <RecipeModal
+            handleCloseModal={this.handleCloseModal}
+            handleShowModal={this.handleShowModal}
+            show={this.state.show}
+            recipeImg={this.state.currentRecipe.image}
+            title={this.state.currentRecipe.title}
+            missedIngredients={this.state.currentRecipe.missedIngredients}
+            recipeObj={this.state.currentRecipe}
+            handlePost={this.handlePost}
+            saved={this.state.saved}
+            handleGetInstructions={this.handleGetInstructions}
+          />}
         {
           this.state.recipes.length > 0 ? (
             this.state.recipes.map((recipe, index) => (
-              <Card key={index}>
-                <Card.Img src={recipe.image} />
-                <Card.Body>
-                  <Card.Title>{recipe.title}</Card.Title>
-                  <RenderModalButton handleShowModal={this.handleShowModal} recipe={recipe}/>
-                </Card.Body>
-              </Card>
+              <Container>
+                <Card key={index}>
+                  <Card.Img src={recipe.image} />
+                  <Card.Body>
+                    <Card.Title>{recipe.title}</Card.Title>
+                    <RenderModalButton handleShowModal={this.handleShowModal} recipe={recipe} />
+                  </Card.Body>
+                </Card>
+              </Container>
             ))
           ) : (
             <h3>No recipes found!</h3>
--- src/SavedRecipes.js
+++ src/SavedRecipes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 
 
 // class SavedRecipes extends React.Component {
